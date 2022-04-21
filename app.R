b27bf502890fdaa058ee2b2fc37ca7f5a6092143
KennyNhan <kendoro123@gmail.com>
2022-04-21 14:19:49 -0500

    adjust app

--- app.R
+++ app.R
@@ -19,8 +19,9 @@ ui <- fluidPage(
               tabPanel("Tab 2", "Movie Recommender", 
                        selectInput(inputId = "Genre", label = "Genre:",  unique(genre_df$genre_name), multiple = TRUE ),
                        submitButton(text = "Search", icon = NULL, width = NULL),
-img(src = "https://image.tmdb.org/t/p/original/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg", height = 300, width = 250),
-img(src = "https://image.tmdb.org/t/p/original/74xTEgt7R36Fpooo50r9T25onhq.jpg", height = 300, width = 250))))
+                       imageOutput("myImage"))))
+#img(src = "https://image.tmdb.org/t/p/original/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg", height = 300, width = 250),
+#img(src = "https://image.tmdb.org/t/p/original/74xTEgt7R36Fpooo50r9T25onhq.jpg", height = 300, width = 250))))
 
 # sliderInput(inputId = "date", 
 #             label = "Release Date Range",
@@ -37,7 +38,31 @@ img(src = "https://image.tmdb.org/t/p/original/74xTEgt7R36Fpooo50r9T25onhq.jpg",
 # submitButton(text = "Create my plot!")
 
 
-server <- function(input, output) {
+server <- function(input, output) {output$myImage <- renderImage({
+    # A temp file to save the output.
+    # This file will be removed later by renderImage
+    movie_base_on_genre <- movie %>% 
+      filter(Genre == input$inputId)
+      #filter(str_detect(Genre, input$inputId))
+    
+    for(x in movie_base_on_genre$Poster_Url){
+      img(src = x, height = 300, width = 250)
+    }
+    #dplyr::filter(movie, grepl(input$inputId, Genre))
+    outfile <- tempfile(fileext = '.jpg')
+    
+    # Generate the PNG
+    jpg(outfile, width = 400, height = 300)
+    hist(rnorm(input$inputId), main = "Generated in renderImage()")
+    dev.off()
+    
+    # Return a list containing the filename
+    list(src = outfile,
+         contentType = 'image/jpg',
+         width = 400,
+         height = 300,
+         alt = "This is alternate text")
+  }, deleteFile = TRUE)
 }
 
 shinyApp(ui = ui, server = server)
