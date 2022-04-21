72d82ee530dc22754069ee4904d9be80ee246f3e
sarahkona47 <89939471+sarahkona47@users.noreply.github.com>
2022-04-21 13:29:56 -0500

    new app

--- app.R
+++ app.R
@@ -1,15 +1,40 @@
 library(shiny)
 library(tidyverse)
 library(ggplot2)
-library(rsconnect)
+movie <- read_csv("https://raw.githubusercontent.com/sarahkona47/Movie-Recommender-using-shiny/2992296bc6ebffdebe766acc1b4704edbc557478/mymoviedb.csv")
 
-Movie <- readr::read_csv("mymoviedb.csv")
+ui <- fluidPage(
+  tabsetPanel(tabPanel("Tab 1", "Explore the Movies ",  
+                       dateRangeInput(inputId = "date", 
+                                      label = "Release Date Range",
+                                      start = as.Date(min(movie$Release_Date)), 
+                                      end = as.Date(max(movie$Release_Date)),
+                                      min = as.Date(min(movie$Release_Date)), 
+                                      max = as.Date(max(movie$Release_Date)), 
+                                      format = "yyyy-mm-dd",
+                                      startview = "month",
+                                      weekstart = 0,
+                                      language = "en",
+                                      separator = " to ")), 
+              tabPanel("Tab 2", "Movie Recommender", 
+                       textInput(inputId = "Genre", label = "Genre:",  unique(movie$Genre), multiple = TRUE ),
+                       submitButton(text = "Search", icon = NULL, width = NULL))))
+# sliderInput(inputId = "date", 
+#             label = "Release Date Range",
+#             min = as.Date(min(movie$Release_Date)), 
+#             max = as.Date(max(movie$Release_Date)), 
+#             value = c(as.Date(min(movie$Release_Date)), as.Date(max(movie$Release_Date)))),
+# textInput("name", 
+#           "Name", 
+#           value = "", 
+#           placeholder = "Lisa"),
+# selectInput("sex", 
+#             "Sex", 
+#             choices = list(Female = "F", Male = "M")),
+# submitButton(text = "Create my plot!")
 
-ui <- fluidPage(textInput(inputId = "Genre", label = "Genre:",  unique(Movie$Genre), multiple = TRUE ),
-                submitButton(text = "search", icon = NULL, width = NULL),
-                plotOutput(outputId = "timeplot"))
 
-server <- function(input, output) { output$timeplot <- renderPlot(Movie %>% 
-                                                                   filter(Genre %in% input$Genre) %>% 
-                                                                    ggplot(aes()))}
+server <- function(input, output) {
+}
+
 shinyApp(ui = ui, server = server)
