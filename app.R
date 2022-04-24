675c28bef506852a6276e0f35ba684873335d290
KennyNhan <kendoro123@gmail.com>
2022-04-23 22:48:26 -0500

    working on tab 2

--- app.R
+++ app.R
@@ -34,12 +34,13 @@ ui <- fluidPage(
                        submitButton(text = "Create my plot!", icon = NULL, width = NULL), 
                        plotOutput(outputId = "movieplot")
                        ), 
-              tabPanel("Tab 2", "Movie Recommender", 
-                       selectInput(inputId = "Genre", label = "Genre:",  unique(genre_df$genre_name), multiple = TRUE ),
+              tabPanel("Search By Genre", "Movie Recommender", 
+                       selectInput(inputId = "Genre", label = "Genre:",  unique(movies_by_genre$Genre1), multiple = TRUE ),
                        submitButton(text = "Search", icon = NULL, width = NULL),
+                       #img(src = "https://image.tmdb.org/t/p/original/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg", height = 300, width = 250),
                        imageOutput("myImage")))
   )
-#img(src = "https://image.tmdb.org/t/p/original/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg", height = 300, width = 250),
+
 #img(src = "https://image.tmdb.org/t/p/original/74xTEgt7R36Fpooo50r9T25onhq.jpg", height = 300, width = 250))))
 
 # sliderInput(inputId = "date", 
@@ -59,43 +60,37 @@ ui <- fluidPage(
 
 server <- function(input, output) {
   output$movieplot <- renderPlot(
-    movies_by_genre %>% 
+    movies_by_genre %>%
       filter(Original_Language == input$lang,
              Genre1 %in% input$genre) %>%
-      arrange(desc(Popularity)) %>% 
-      select(Title, Popularity) %>% 
-      top_n(20) %>% 
+      arrange(desc(Popularity)) %>%
+      select(Title, Popularity) %>%
+      top_n(20) %>%
       ggplot(aes(x = Popularity, y = fct_reorder(Title, Popularity))) +
       geom_col(fill = "lightblue") +
       labs(title = "Top Movies in Selected Language and Genre", y = "", caption = "Popularity metric is computed by TMDB developers based on the number of views per day, \nvotes per day, number of users marked it as 'favorite' and 'watchlist' for the data, release date and more other metrics") +
       theme(plot.caption = element_text(hjust = 0.5)) +
       theme_minimal()
-  ) 
-  output$myImage <- renderImage({
-    # A temp file to save the output.
-    # This file will be removed later by renderImage
-    movie_base_on_genre <- movie %>% 
-      filter(Genre == input$inputId)
-    #filter(str_detect(Genre, input$inputId))
+  )
+  output$myImage <- renderImage(
     
-    for(x in movie_base_on_genre$Poster_Url){
-      img(src = x, height = 300, width = 250)
-    }
-    #dplyr::filter(movie, grepl(input$inputId, Genre))
-    outfile <- tempfile(fileext = '.jpg')
+   movies_by_genre %>% 
+      filter(Genre1 %in% input$Genre) %>%
+      arrange(desc(Popularity)) %>% 
+      select(Poster_Url, Popularity) %>% 
+      top_n(1) %>% 
+      img(src = Poster_Url, height = 300, width = 250))
     
-    # Generate the PNG
-    jpg(outfile, width = 400, height = 300)
-    hist(rnorm(input$inputId), main = "Generated in renderImage()")
-    dev.off()
+    #   outfile <- tempfile(fileext=movies_by_genre$Poster_Url)
+    #   jpeg(outfile, width=250, height=300)
+    #   #dev.off()
+    #   
+    #   # Return a list
+    #   list(src = outfile,
+    #        alt = "This is alternate text")
+    # }, deleteFile = FALSE)
     
-    # Return a list containing the filename
-    list(src = outfile,
-         contentType = 'image/jpg',
-         width = 400,
-         height = 300,
-         alt = "This is alternate text")
-  }, deleteFile = TRUE)
+
 }                       
 
 shinyApp(ui = ui, server = server)
